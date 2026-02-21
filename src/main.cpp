5101589a23fcb6ab216fdddf3f711e6923a9fe32
mohan2007-coder <mohan.sharma_cs.aiml24@gla.ac.in>
2026-02-21 21:29:48 +0530

    Define RGB LED pins for PWM control

--- src/main.cpp
+++ src/main.cpp
@@ -1,34 +1,41 @@
 #include <Arduino.h>
+
 /**
  * @file main.cpp
  * @brief Embedded RGB LED Control (Digital + PWM)
- * @author YOUR_NAME
- * @date YYYY-MM-DD
- *
- * @details
+ * @author Mohan Sharma
+ *  @date YYYY-MM-DD
  * Controls RGB LED using digital ON/OFF
  * and analog PWM brightness control.
  */
 
- // TODO 1:
- // Define RED pin (Use 9)
+// TODO 1:
+// Define RED pin (Use 9)
+const int RED_PIN = 9;
 
- // TODO 2:
- // Define GREEN pin (Use 10)
+// TODO 2:
+// Define GREEN pin (Use 10)
+const int GREEN_PIN = 10;
 
- // TODO 3:
- // Define BLUE pin (Use 11)
+// TODO 3:
+// Define BLUE pin (Use 11)
+const int BLUE_PIN = 11;
 
 void setup() {
 
     // TODO 4:
     // Initialize Serial communication (9600 baud)
+    Serial.begin(9600);
 
     // TODO 5:
     // Configure RGB pins as OUTPUT
+    pinMode(RED_PIN, OUTPUT);
+    pinMode(GREEN_PIN, OUTPUT);
+    pinMode(BLUE_PIN, OUTPUT);
 
     // TODO 6:
     // Print initialization message
+    Serial.println("=== RGB LED Control System Initialized ===");
 }
 
 void loop() {
@@ -37,21 +44,35 @@ void loop() {
 
     // TODO 7:
     // Turn ON red (digital HIGH)
+    digitalWrite(RED_PIN, HIGH);
+    delay(1000);
 
     // TODO 8:
     // Turn OFF red
+    digitalWrite(RED_PIN, LOW);
+    delay(500);
 
     // -------- ANALOG (PWM) MODE --------
 
     // TODO 9:
     // Set RED brightness using analogWrite()
+    analogWrite(RED_PIN, 255);   // full brightness
 
     // TODO 10:
     // Set GREEN brightness using analogWrite()
+    analogWrite(GREEN_PIN, 100); // medium brightness
 
     // TODO 11:
     // Set BLUE brightness using analogWrite()
+    analogWrite(BLUE_PIN, 50);   // low brightness
 
     // TODO 12:
     // Add delay for visible transition
+    delay(2000);
+
+    // turn off all colors before next cycle
+    analogWrite(RED_PIN, 0);
+    analogWrite(GREEN_PIN, 0);
+    analogWrite(BLUE_PIN, 0);
+    delay(1000);
 }
