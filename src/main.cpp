b86caa3adec4dc12aa046635eef7af1c1f8e20ac
mohan2007-coder <mohan.sharma_cs.aiml24@gla.ac.in>
2026-02-21 21:31:08 +0530

    Improve color transition timing and reset cycle

--- src/main.cpp
+++ src/main.cpp
@@ -24,7 +24,7 @@ const int BLUE_PIN = 11;
 void setup() {
 
     // TODO 4:
-    // Initialize Serial communication (9600 baud)
+    // Initialize Serial communication(9600 baud)
     Serial.begin(9600);
 
     // TODO 5:
